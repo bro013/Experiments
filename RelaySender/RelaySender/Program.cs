a84e3d86ab930cc201439d820226c3847d98d0b6
Bjørn Rosland <bjoernrosland@gmail.com>
2019-01-08 20:40:07 +0100

    Added RelaySender

---
+++ RelaySender/RelaySender/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelaySender
+{
+    class Program
+    {
+        private const string RelayNamespace = "roslanden.servicebus.windows.net";
+        private const string ConnectionName = "relayhybrid";
+        private const string KeyName = "Listen";
+        private const string Key = "qlHU2J9TmjhPoKVtXbAzzBgmtULMFzH/KyphGAm5WpI=";
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
