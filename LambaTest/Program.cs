b2f1f29e7822b2bdb7444a888fd1cd2687979557
bro013 <bjoernrosland@gmail.com>
2019-01-29 19:37:32 +0100

    More lambda functions

--- LambaTest/Program.cs
+++ LambaTest/Program.cs
@@ -4,9 +4,22 @@ namespace LambaTest
 {
     class Program
     {
+        private delegate int squareFunc(int i);
+        private delegate int sumFunc(int i, int j);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            squareFunc squared = x => x*x;
+            int y = 5;
+            int z = squared(5);
+            Console.WriteLine($"{y} squred is equal to {z}");
+
+            sumFunc sum = (a,b) => a + b;
+            int x1 = 1;
+            int x2 = 3;
+            int x3 = sum(x1, x2);
+            Console.WriteLine($"The sum of {x1} and {x2} is {x3}");
+
         }
     }
 }
