d9bd74710e31f2b839901ba5f2096e040c125a98
Bjørn Rosland <bjoernrosland@gmail.com>
2019-02-25 21:09:58 +0100

    [AwesomeSauceApi] Example with endpoints

--- API/AwesomeSouceApi/AwesomeSauceApi/AwesomeSauceApi/Startup.cs
+++ API/AwesomeSouceApi/AwesomeSauceApi/AwesomeSauceApi/Startup.cs
@@ -20,15 +20,41 @@ namespace AwesomeSauceApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            /* Enpoints
+            app.Use(async (context, next) =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+                if(context.Request.Path == "/foo")
+                {
+                    await context.Response.WriteAsync($"Welcome to Foo");
+                }
+                else
+                {
+                    await next();
+                }
             });
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/bar")
+                {
+                    await context.Response.WriteAsync($"Welcome to Bar");
+                }
+                else
+                {
+                    await next();
+                }
+            }); 
+            app.Run(async (context) => await context.Response.WriteAsync($"Welcome to the default"));
+            */
+
+            /*Enppoints using Map and MapWhen */
+
+            app.Map("/foo", config => config.Use(async (context, next) => await context.Response.WriteAsync("Welcome to /foo")));
+
+            app.MapWhen(context => context.Request.Method == "POST" && context.Request.Path == "/bar",
+                config => config.Use(async (context, next) => await context.Response.WriteAsync("Welcome to POST /bar")));
+
+            app.Run(async (context) => await context.Response.WriteAsync($"Welcome to the default"));
         }
     }
 }
